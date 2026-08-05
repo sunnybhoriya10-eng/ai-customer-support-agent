252048d1e1b347ceeaf232c35ff1cd64cf80e6f8
Sunny <sunnybhoriya10@gmail.com>
2026-08-05 14:43:43 +0530

    Fix dependencies and Prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,1 @@
-import { PrismaClient } from "@prisma/client";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
-
-const adapter = new PrismaBetterSqlite3({
-  url: "./prisma/dev.db",
-});
-
-export const prisma = new PrismaClient({
-  adapter,
-});
+export {};
