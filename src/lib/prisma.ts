7132dc7e90e53a4c7f163101bd0a5baf802d5789
Sunny <sunnybhoriya10@gmail.com>
2026-08-05 14:45:25 +0530

    Fix Prisma and dependencies

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,1 +1,11 @@
-export {};
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
